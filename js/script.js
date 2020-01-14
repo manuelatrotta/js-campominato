6e0b85a53dc6d18455f6d04bf6efc607d4f43d0b
manuelatrotta <manuelat88@hotmail.it>
2020-01-14 18:20:46 +0100

    inizio sistemazione schema risolutivo

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ alert('campominato');
 //l computer deve generare 16 numeri casuali da 1 a 100.In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 //step 1 : il pc deve generare 16 numeri random da 1 a 100
-var randomNumbersPc = [56];
-for( i=1; i<=16; i++) {
-  numberCreate = getRandom (1, 100);
-  randomNumbersPc.push(numberCreate);
+var randomNumbersPc = [];
+var arrayUserTries = [];
+var findNumber = false;
+var maxTries = 84;
+
+while (randomNumbersPc.length < 16) {
+  var cpuRandomNumber = getRandom(1, 100);
+  if(randomNumbersPc.includes(cpuRandomNumber) == false) {
+    randomNumbersPc.push(cpuRandomNumber);
+  }
 }
 console.log(randomNumbersPc);
 function getRandom (min, max) {
@@ -14,27 +20,17 @@ function getRandom (min, max) {
   var random = Math.floor(Math.random() * (max - min + min) + 1);
   return random;
 }
-//step 2 chiedere 84 numeri all'utente
-var listUser = [ ];
-for (var i = 1; i <= 8; i++) {
-  numberUser = prompt('inserisci 84 numeri differenti da 1 a 100');
-  listUser.push(numberUser);
-}
-console.log(listUser);
-//step 3: se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
-var numberPresent = false;
-for (var i=0; i<randomNumbersPc.length; i++){
-  var present  = randomNumbersPc[i];
-  if(numberUser == present){
-    numberPresent = true;
+function checkBomb(arrayToCheck, numberToCheck) {
+  for(var i =0; i < arrayToCheck.length; i++) {
+    if (numberToCheck == arrayToCheck[i]) {
+      return true;
+    }
   }
+  return false;
 }
-//comunicare il responso all'utente
-console.log(numberPresent);
-if (numberPresent == false) {
-  console.log('numero non presente,continua a giocare');
-  numberUser = prompt('inserisci 84 numeri differenti da 1 a 100');
-}else{
-  console.log('game over');
 
+var counte = 0;
+while(arrayUserTries.length < maxTries && checkBomb(arrayRandom, userTry) == false){
+  var userTry = parseInt(prompt('inserisci un numero da 1 a 100'));
+  //controllo che sia una bomba
 }
