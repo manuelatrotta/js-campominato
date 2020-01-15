d97d1abed68f22a27e4e6a95dcc732b1cab06cb1
manuelatrotta <manuelat88@hotmail.it>
2020-01-15 20:34:32 +0100

    sistemazione commenti

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ alert('campominato');
 //l computer deve generare 16 numeri casuali da 1 a 100.In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 //step 1 : il pc deve generare 16 numeri random da 1 a 100
+//variabili
 var randomNumbersPc = [];
+var arrayUserTries = [];
+var counte = 0;
+var findNumber = false;
+var maxTries = 5;
+message = 'you win';
 
 while (randomNumbersPc.length <= 16) {
   var cpuRandomNumber = getRandom(1, 100);
@@ -11,13 +17,14 @@ while (randomNumbersPc.length <= 16) {
   }
 }
 console.log(randomNumbersPc.sort());
-
+//funzione per numero random
 function getRandom (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   var random = Math.floor(Math.random() * (max - min + min) + 1);
   return random;
 }
+//funzione in cui si verifica se il numero da cercare è presente nell'array
 function checkBomb(arrayToCheck, numberToCheck) {
   for(var i =0; i < arrayToCheck.length; i++) {
     if (numberToCheck == arrayToCheck[i]) {
@@ -34,20 +41,16 @@ function checkRangeNumber(min, max, number) {
   }
   return result;
 }
-
-var arrayUserTries = [];
-var counte = 0;
-message = 'you win';
-var findNumber = false;
-var maxTries = 4;
+//condizioni che verificano se il numero inserito dall'utente è diverso da quelli random generati dal pc, se viene inserito un numero sempre diverso.
+//se il numero inserito dall'utente è diverso allora vince,altrimenti perde. Con counte otteniamo il punteggio.
 
 while (arrayUserTries.length < maxTries && findNumber == false) {
   var userNumber = false;
   do{
     var userNumber = parseInt(prompt('inserisci un numero da 1 a 100'));
     console.log('numero inserito' + checkRangeNumber(1, 100, userNumber));
-  } while(checkRangeNumber(1, 100, userNumber) == false)
-  if(checkBomb(arrayUserTries, userNumber) == false) {
+  }while(checkRangeNumber(1, 100, userNumber) == false)
+    if(checkBomb(arrayUserTries, userNumber) == false) {
     arrayUserTries.push(userNumber);
     // se il numero dell'utente è presente nelle numberBomb hai perso
     if(checkBomb(randomNumbersPc, userNumber) == true){
@@ -58,5 +61,6 @@ while (arrayUserTries.length < maxTries && findNumber == false) {
     }
   }
 }
+//stampa su console numeri inseriti da utente, messaggio e punteggio
 console.log(arrayUserTries);
 console.log(message + ' ' + counte);
